43d8f35429611d2f8a821dd4e038529bc6e7a658
amanda03022 <amandamaria.dornelas@yahoo.com.br>
2023-12-12 11:40:56 -0300

    personalize att

--- personalize.php
+++ personalize.php
@@ -1,188 +1,312 @@
+[11:14, 12/12/2023] Duda Marioza :
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UNIQUE Personalize</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fabric.js/3.6.2/fabric.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https:…
+[11:24, 12/12/2023] Duda Marioza : [11:21, 12/12/2023] Maria Eduarda: AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+[11:21, 12/12/2023] Maria Eduarda: BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+[11:21, 12/12/2023] Maria Eduarda: CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC…
+[11:37, 12/12/2023] Duda Marioza : <!DOCTYPE html>
+<html lang=" pt-br">
 
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,1,0" />
-    <link rel="stylesheet" href="personalizestyle.css">
-    
-    <title>Paint</title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>UNIQUE Personalize</title>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/fabric.js/3.6.2/fabric.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Caveat:wght@600&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Herr+Von+Muellerhoff&display=swap" rel="stylesheet">
+        <style>
+            body {
+                display: flex;
+                margin: 0;
+                font-family: 'Arial', sans-serif;
+            }
+
+            #canvas-container {
+                flex: 1;
+                display: flex;
+                justify-content: flex-start;
+                align-items: center;
+                padding: 20px;
+            }
+
+            #canvas {
+                border: 1px solid black;
+            }
+
+            #toolbox {
+                box-sizing: border-box;
+                background-color: #f0f0f0;
+                padding: 60px;
+                border-radius: 10px;
+                margin-left: 40px;
+                margin-top: 60px;
+
+            }
+
+            label {
+                display: block;
+                margin-bottom: 8px;
+            }
+
+            input[type="color"],
+            input[type="text"],
+            select,
+            input[type="range"] {
+                width: 100%;
+                padding: 8px;
+                margin-bottom: 16px;
+                box-sizing: border-box;
+                border: 1px solid #ccc;
+                border-radius: 8px;
+            }
+
+            .color-picker {
+                width: 100%;
+                padding: 8px;
+                margin-bottom: 16px;
+                box-sizing: border-box;
+                border: 1px solid #ccc;
+                border-radius: 50%;
+            }
+
+            button {
+                border-radius: 50%;
+                padding: 10px;
+                margin: 5px;
+                cursor: pointer;
+                border: none;
+                background-color: #ccc;
+                font-size: 16px;
+            }
+
+            button.drawing-mode {
+                background-color: #e74c3c;
+                /*  Cor do botão mudando para vermelho */
+                color: white;
+            }
+
+            button i {
+                font-size: 24px;
+            }
+
+            .editable {
+                cursor: text;
+            }
+
+            .delete-button {
+                position: absolute;
+                top: 0;
+                left: 0;
+                cursor: pointer;
+                font-size: 16px;
+                color: red;
+                background-color: white;
+                border: none;
+                padding: 5px;
+                margin: 5px;
+                z-index: 1;
+            }
+        </style>
+    </head>
+
 <body>
+    <?php
+    include_once("menu.php");
+    ?>
 
-    <section class="container">
+    <div id="canvas-container">
+        <canvas id="canvas" width="300" height="450"></canvas>
 
-<!--teste-->        <canvas id="myCanvas" width="900" height="600"></canvas>
+        <div id="toolbox">
+            <label for="text">Texto:</label>
+            <input type="text" id="text" name="text" placeholder="Digite seu texto" onkeydown="handleKey(event)">
 
-        <div>
-            <section class="tool-box">
-                <button class="button__tool active" data-action="brush">
-                    <span class="material-symbols-outlined">brush</span>
-                </button>
+            <label for="color">Cor:</label>
+            <input type="color" id="color" name="color" value="#ff0000" oninput="updateTextColor()">
 
-                <button class="button__tool" data-action="rubber">
-                    <span class="material-symbols-outlined">ink_eraser</span>
-                </button>
+            <label for="font">Fonte:</label>
+            <select id="font" name="font" onchange="updateTextFont()">
+                <option value="Arial">Arial</option>
+                <option value="Times New Roman">Times New Roman</option>
+                <option value="Caveat">Caveat</option>
+                <option value="Dancing Script">Dancing Script</option>
+                <option value="Herr Von Muellerhoff">Herr Von Muellerhoff</option>
+            </select>
 
-                <button class="button__tool" data-action="text">
-                    <span class="material-symbols-outlined">title</span>
-                </button>
 
-                <button class="button__tool">
-                    <input type="color" class="input__color">
-                </button>
-            </section>
 
-            <section class="tool-box">
-                <button class="button__size" data-size=5>
-                    <span class="stroke"></span>
-                </button>
+            <button id="brushButton" onclick="toggleDrawingMode()">
+                <i class="fas fa-paint-brush"></i>
+            </button>
 
-                <button class="button__size" data-size=10>
-                    <span class="stroke"></span>
-                </button>
+            <label for="brushSize">Tamanho do Pincel:</label>
+            <input type="range" id="brushSize" name="brushSize" min="1" max="50" value="5">
 
-                <button class="button__size active" data-size=20>
-                    <span class="stroke"></span>
-                </button>
+            <label for="brushColor">Cor do Pincel:</label>
+            <input type="color" id="brushColor" name="brushColor" value="#000000">
 
-                <button class="button__size" data-size=30>
-                    <span class="stroke"></span>
-                </button>
-            </section>
-            
-            <section class="tool-box">
-                <button class="button__tool button__clear">
-                    <span class="material-symbols-outlined">delete</span>
-                </button>
-            </section>
+            <button onclick="deleteSelected()">
+                <i class="fas fa-trash"></i>
+            </button>
         </div>
-
-    </section>
+    </div>
     <script>
-        const canvas = document.getElementById("myCanvas");
-        const ctx = canvas.getContext("2d");
+        const canvas = new fabric.Canvas('canvas');
+        let drawingMode = false;
 
-        const inputColor = document.querySelector(".input__color");
-        const tools = document.querySelectorAll(".button__tool");
-        const sizeButtons = document.querySelectorAll(".button__size");
-        const buttonClear = document.querySelector(".button__clear");
-
-        let brushSize = 20;
-        let isPainting = false;
-        let activeTool = "brush";
-        let lastX, lastY;
-
-        inputColor.addEventListener("change", ({ target }) => {
-            ctx.fillStyle = target.value;
+        fabric.Image.fromURL('canecaa.webp', function (img) {
+            img.scaleToWidth(canvas.width);
+            canvas.add(img);
+            img.set({ selectable: false });
         });
 
-        canvas.addEventListener("mousedown", ({ clientX, clientY }) => {
-            isPainting = true;
+        document.addEventListener('keydown', function (event) {
+            handleKey(event);
+        });
 
-            if (activeTool === "brush" || activeTool === "text") {
-                draw(clientX, clientY);
-            }
-
-            if (activeTool === "rubber") {
-                erase(clientX, clientY);
+        canvas.on('mouse:dblclick', function (options) {
+            const target = options.target;
+            if (target && target.type === 'i-text') {
+                editText(target);
             }
         });
 
-        canvas.addEventListener("mousemove", ({ clientX, clientY }) => {
-            if (isPainting) {
-                if (activeTool === "brush" || activeTool === "text") {
-                    draw(clientX, clientY);
+        function handleKey(event) {
+            if (event.key === 'Enter') {
+                // Verificar se há texto antes de chamar addText
+                const textValue = document.getElementById('text').value.trim();
+                if (textValue !== '') {
+                    addText();
                 }
-
-                if (activeTool === "rubber") {
-                    erase(clientX, clientY);
-                }
+            } else if (event.key === 'Delete' || event.key === 'Backspace') {
+                deleteSelected();
             }
-        });
+        }
 
-        canvas.addEventListener("mouseup", () => {
-            isPainting = false;
-            lastX = undefined;
-            lastY = undefined;
-        });
+        function toggleDrawingMode() {
+            drawingMode = !drawingMode;
+            canvas.isDrawingMode = drawingMode;
+            canvas.selection = !drawingMode;
 
-        const draw = (x, y) => {
-            ctx.globalCompositeOperation = "source-over";
-            ctx.lineWidth = brushSize;
-            ctx.lineCap = "round";
-            ctx.strokeStyle = ctx.fillStyle;
-
-            if (activeTool === "text") {
-                const text = prompt("Enter text:");
-                if (text) {
-                    ctx.font = `${brushSize}px Arial`;
-                    ctx.fillText(text, x - canvas.offsetLeft, y - canvas.offsetTop);
-                }
+            const brushButton = document.getElementById('brushButton');
+            if (drawingMode) {
+                brushButton.classList.add('drawing-mode');
             } else {
-                if (!lastX || !lastY) {
-                    ctx.beginPath();
-                    ctx.moveTo(x - canvas.offsetLeft, y - canvas.offsetTop);
+                brushButton.classList.remove('drawing-mode');
+            }
+        }
+
+        function updateBrushSize() {
+            const brushSize = document.getElementById('brushSize').value;
+            canvas.freeDrawingBrush.width = parseInt(brushSize, 10);
+        }
+
+        function updateBrushColor() {
+            const brushColor = document.getElementById('brushColor').value;
+            canvas.freeDrawingBrush.color = brushColor;
+        }
+
+        function addText() {
+            const color = document.getElementById('color').value;
+            const textValue = document.getElementById('text').value;
+            const font = document.getElementById('font').value;
+
+            const newText = new fabric.IText(textValue, {
+                fontSize: 24,
+                left: 50,
+                top: 50,
+                fill: color,
+                fontFamily: font,
+                selectable: true,
+                editable: true,
+                class: 'editable', // Adicionando a classe para indicar que é editável
+            });
+
+            canvas.add(newText);
+            canvas.setActiveObject(newText);
+            clearTextInputs();
+        }
+
+        function editText(textObject) {
+            const selectionStart = textObject.getSelectionStart();
+            const selectionEnd = textObject.getSelectionEnd();
+
+            if (selectionStart === selectionEnd) {
+                // Se a seleção estiver vazia, deleta a letra anterior
+                textObject.setSelectionStart(selectionStart - 1);
+            }
+
+            textObject.enterEditing(); // Inicia o modo de edição
+        }
+
+        function updateTextColor() {
+            const color = document.getElementById('color').value;
+            const activeObject = canvas.getActiveObject();
+            if (activeObject && activeObject.type === 'i-text') {
+                activeObject.set('fill', color);
+                canvas.renderAll();
+            }
+        }
+
+        function updateTextFont() {
+            const font = document.getElementById('font').value;
+            const activeObject = canvas.getActiveObject();
+            if (activeObject && activeObject.type === 'i-text') {
+                activeObject.set('fontFamily', font);
+                canvas.renderAll();
+            }
+        }
+
+        function deleteSelected() {
+            const activeObject = canvas.getActiveObject();
+            if (activeObject) {
+                if (activeObject.type === 'i-text' && activeObject.isEditing) {
+                    // Se estiver editando texto, deleta a seleção dentro do texto
+                    const selectionStart = activeObject.getSelectionStart();
+                    const selectionEnd = activeObject.getSelectionEnd();
+                    activeObject.removeChars(selectionStart, selectionEnd - selectionStart);
                 } else {
-                    ctx.lineTo(x - canvas.offsetLeft, y - canvas.offsetTop);
-                    ctx.stroke();
+                    // Deleta o objeto selecionado
+                    canvas.remove(activeObject);
                 }
             }
+        }
 
-            lastX = x - canvas.offsetLeft;
-            lastY = y - canvas.offsetTop;
-        };
+        function clearTextInputs() {
+            document.getElementById('text').value = '';
+            document.getElementById('color').value = '#000000';
+            document.getElementById('font').value = 'Arial';
+        }
 
-        const erase = (x, y) => {
-            ctx.globalCompositeOperation = "destination-out";
-            ctx.lineWidth = brushSize;
-            ctx.lineCap = "round";
-
-            if (!lastX || !lastY) {
-                ctx.beginPath();
-                ctx.moveTo(x - canvas.offsetLeft, y - canvas.offsetTop);
-            } else {
-                ctx.lineTo(x - canvas.offsetLeft, y - canvas.offsetTop);
-                ctx.stroke();
-            }
-
-            lastX = x - canvas.offsetLeft;
-            lastY = y - canvas.offsetTop;
-        };
-
-        const selectTool = ({ target }) => {
-            const selectedTool = target.closest("button");
-            const action = selectedTool.getAttribute("data-action");
-
-            if (action) {
-                tools.forEach((tool) => tool.classList.remove("active"));
-                selectedTool.classList.add("active");
-                activeTool = action;
-            }
-        };
-
-        const selectSize = ({ target }) => {
-            const selectedTool = target.closest("button");
-            const size = selectedTool.getAttribute("data-size");
-
-            sizeButtons.forEach((tool) => tool.classList.remove("active"));
-            selectedTool.classList.add("active");
-            brushSize = size;
-        };
-
-        tools.forEach((tool) => {
-            tool.addEventListener("click", selectTool);
+        document.getElementById('brushSize').addEventListener('input', function () {
+            updateBrushSize();
         });
 
-        sizeButtons.forEach((button) => {
-            button.addEventListener("click", selectSize);
-        });
-
-        buttonClear.addEventListener("click", () => {
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
+        document.getElementById('brushColor').addEventListener('input', function () {
+            updateBrushColor();
         });
     </script>
-    <script src="./script.js"></script>
 </body>
+
 </html>
